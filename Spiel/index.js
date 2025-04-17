2b2594c90b3c88952c1405a774a3c04a74ba9e14
Henne98 <hendrik.springinsgut.2023@leibniz-fh.de>
2025-04-17 14:07:07 +0200

    Add files via upload

---
+++ Spiel/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static("./public"));
+
+let port = process.env.PORT || 3000;
+const server = app.listen(port)
+
+const io = require("./io/io.js")(server);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/pages/Startseite/index.html");
+})
+
+app.get("/lobby", (req, res) => {
+    res.sendFile(__dirname + "/public/pages/lobby.htm");
+})
+
+app.get("/anleitung", (req, res) => {
+    res.sendFile(__dirname + "/public/pages/Spielanleitung-MoveIt.html");
+})
+
+//
