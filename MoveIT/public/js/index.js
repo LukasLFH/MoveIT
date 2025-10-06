f9bd481bf4ae431808f950fee3a88b3abd7bda91
Lukas Habermann <l26973@eon.com>
2025-10-06 10:26:39 +0200

    add comments to HTML and JS files

--- MoveIT/public/js/index.js
+++ MoveIT/public/js/index.js
@@ -1,20 +1,25 @@
 function showJoinPopup() {
+    // Popup für den Lobby-Beitrit
     document.getElementById('joinOverlay').style.display = 'flex';
 }
 
 function showOptionsPopup() {
+    // Popup für die Optionen
     document.getElementById('optionsOverlay').style.display = 'flex';
 }
 
 function closePopup(id) {
+    // Funktion um Popups zu schließen
     document.getElementById(id).style.display = 'none';
 }
 
 function createLobby() {
+    // Funktion um Lobby zu erstellen
     window.location.href = 'lobby_host';
 }
 
 function joinLobby() {
+    // Funktion um Lobby beizutreten
     const inputCode = document.getElementById('joinCodeInput').value.trim().toUpperCase();
     const savedCode = localStorage.getItem('lobbyCode');
 
